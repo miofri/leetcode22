b8ab1338a28e420542cccbc7e187cf6dee3358ff
Selvi <74725778+miofri@users.noreply.github.com>
2022-09-06 20:36:41 +0300

    Create longest-substring-without-repeating-characters.js

---
+++ longest-substring-without-repeating-characters.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+	let spreadStr = [...s]
+	let maxLen = Number.MIN_SAFE_INTEGER
+	let subStr = []
+
+	if (spreadStr.length === 0 || s === "") {
+		return 0
+	}
+
+	for (let index = 0; index < spreadStr.length; index++) {
+
+		if (subStr.includes(spreadStr[index])) {
+			index = index - subStr.length + 1
+			while (subStr.length > 0) {
+				subStr.pop()
+			}
+		}
+		subStr.push(spreadStr[index])
+		if (subStr.length > maxLen) {
+			maxLen = subStr.length
+		}
+	}
+	return (maxLen)
+};
